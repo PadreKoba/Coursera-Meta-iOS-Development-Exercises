ce41b38019ea0ba2b909a6e5d4623400f27f97f0
PadreKoba <kobasinauridze@outlook.com>
2025-12-12 00:53:23 +0400

    add classes exercise

---
+++ Classes.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class LocalFile {
+    let name: String
+    let fileExtension: String
+    init(name: String, fileExtension: String) {
+        self.name = name
+        self.fileExtension = fileExtension
+    }
+    var fullFileName: String {
+        name + fileExtension
+    }
+}
+let file = LocalFile(name: "image", fileExtension: ".png")
+print(file.fullFileName)
